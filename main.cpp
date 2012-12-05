5850a6d0e6f8a96614f43721db00c5cbf0a38373
Michiel Vos <info@fastfox.nl>
2012-12-05 13:14:09 +0100

    new

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 
 #include "shader.h"
 
+#include <time.h>
+
 struct particle {
 	glm::vec3 location;
 	
@@ -62,7 +64,7 @@ void Control::initialize() {
 	// SDL
 	SDL_Init(SDL_INIT_EVERYTHING); 
 	SDL_SetVideoMode(iWidth, iHeight, 32, SDL_OPENGL | SDL_GL_DOUBLEBUFFER);
-	SDL_EnableKeyRepeat(10, 10);
+	SDL_EnableKeyRepeat(1, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 32);
 	SDL_WM_SetCaption("Computer Graphics Final Project 2012", NULL);		
 
@@ -71,7 +73,7 @@ void Control::initialize() {
 
 	// GL
 	glViewport(0, 0, iWidth, iHeight);
-	glClearColor(0.0f, 0.5f, 1.0f, 1.0f); 
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
 	glEnable(GL_DEPTH_TEST);
 	//glEnable(GL_CULL_FACE);
 	glDepthMask(GL_TRUE);
@@ -188,6 +190,36 @@ void Control::render() {
 
 	float up = 0.0f;
 	float rotation = 0.0f;
+	float y[10];
+	float x[10];
+
+	float hi[8];
+
+	int max = 5;
+
+	float circlex[360], circley[360];
+	for(int i = 0; i < 360; i++) {
+		circlex[i] = 0; 
+		circley[i] = 0;
+	}
+	
+	int newX, newY;
+	float speed = 1.0f;
+
+
+	float fCamRoty = 0.0f;
+	float fCamZ = 5.0f;
+
+
+	bool go = false;
+
+	for(int i = 0; i < 10; i++) {
+		y[i] = 0.0f;
+		x[i] = -(float)i / 1.5;
+		if(i <= 8) {
+			hi[i] = 0.0f;
+		}
+	}
 
 	while(running) {
 		SDL_Event event;
@@ -207,12 +239,25 @@ void Control::render() {
 						up += 0.15f;
 					} else if(event.key.keysym.sym == SDLK_DOWN) {
 						up -= 0.15f;
+					}	else if(event.key.keysym.sym == 'a') {
+						fCamRoty += 1.0f;
+					} else if(event.key.keysym.sym == 'd') {
+						fCamRoty -= 1.0f;
+					} else if(event.key.keysym.sym == 'w') {
+						fCamZ -= 0.1f;
+					} else if(event.key.keysym.sym == 's') {
+						fCamZ += 0.1f;
+					} else if(event.key.keysym.sym == 'g') {
+						go = true;
 					}
+					//std::cout << event.key.keysym.sym << std::endl;
 					break;
 			}
 		}
 
-		mViewModel = glm::lookAt(glm::vec3(-1.0f, 1.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		mViewModel = glm::lookAt(glm::vec3(-1.0f, 1.0f, fCamZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		mViewModel = glm::rotate(mViewModel, fCamRoty, glm::vec3(0.0f, 1.0f, 0.0f));
 		mProj = glm::perspective(45.0f, (float) iWidth / (float) iHeight, 0.1f, 100.0f);
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
@@ -222,29 +267,126 @@ void Control::render() {
 		glUniformMatrix4fv(iViewModelLoc, 1, GL_FALSE, glm::value_ptr(mViewModel));
 
 
-		glm::mat4 mCurrent;
+		glm::mat4 mCurrent, temp;
 
 
 		// Triangles
 		glBindVertexArray(uiVAO[0]);
 
+		// beam
 
 		mCurrent = glm::rotate(mViewModel, rotation, glm::vec3(0.0f, 1.0f, 0.0f));
 		mCurrent = glm::scale(mCurrent, glm::vec3(0.2f, 0.2f, 0.2f));
 		mCurrent = glm::translate(mCurrent, glm::vec3(0.0f, up, 0.0f));
 
-		for(int i = 0; i < 25; i++) {
-			mCurrent = glm::translate(mCurrent, glm::vec3(1.1f, 0.0f, 0.0f));
-			glm::mat4 temp = glm::rotate(mCurrent, rotation * i, glm::vec3(1.0f, 0.0f, 0.0f));
+		for(int i = 0; i < 500; i++) {
+			mCurrent = glm::translate(mCurrent, glm::vec3(0.2f, 0.0f, 0.0f));
+			if(i % 2 == 0) {
+				//temp = glm::translate(mCurrent, glm::vec3(-1.0f, 0.0f, 0.0f));
+				temp = mCurrent;
+			} else {
+				temp = mCurrent;
+			}
+			temp = glm::rotate(temp, rotation * i, glm::vec3(1.0f, 0.0f, 0.0f));
 	    glUniformMatrix4fv(iViewModelLoc, 1, GL_FALSE, glm::value_ptr(temp)); 
 			glDrawArrays(GL_TRIANGLES, 0, 3);
 		}
+
 		
+		// big triangle
+
 		mCurrent = glm::translate(mViewModel, glm::vec3(0.0f, -1.0f, 0.0f));
+		mCurrent = glm::translate(mViewModel, glm::vec3( (float)(rand() % 5) / 100.0f, (float)(rand() % 5) / 200.0f -1.0f, 0.0f));
     glUniformMatrix4fv(iViewModelLoc, 1, GL_FALSE, glm::value_ptr(mCurrent)); 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		// Lines
+		// upmoving triangles
+
+		mCurrent = glm::scale(mViewModel, glm::vec3(0.2f, 0.2f, 0.2f));
+		for(int i = 0; i < 10; i++) {
+			temp = glm::translate(mCurrent, glm::vec3( x[i] + (float)(rand() %5) / 50.0f, y[i], 0.0f));
+			if(y[i] > 5.0f) {
+				if(y[i] > 10.0f) {
+					y[i] = 0.0f;
+				} else {
+					if(i >= 5) {
+						//x[i] -= 0.01f;
+					} else {
+						//x[i] += 0.01f;
+					}
+					temp = glm::rotate(temp, (float) i * y[i] * 100, glm::vec3(0.0f, 0.0f, 1.0f));
+					y[i] += 0.01f;
+				}
+			} else {
+				y[i] += 0.05f;
+			}
+    	glUniformMatrix4fv(iViewModelLoc, 1, GL_FALSE, glm::value_ptr(temp)); 
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+		}
+
+
+		//
+		
+		mCurrent = glm::scale(mViewModel, glm::vec3(0.1f, 0.1f, 0.1f));
+		mCurrent = glm::translate(mCurrent, glm::vec3( -10.0f, -10.0f, 0.0f));
+		max = 10;	
+		
+		if(go) {
+			for(int i = 1; i <= max; i++ ) {
+			
+				for(int j = 0; j < max; j++) {
+					temp = glm::translate(mCurrent, glm::vec3( (float)i , (float)j, 0.0f));
+					//temp = mCurrent;
+					if(i > max/2){
+						if(j > max/2) {
+							hi[0] += 0.001f;
+							hi[1] += 0.001f;
+							temp = glm::translate(temp, glm::vec3(hi[0], hi[1], 0.0f));
+						} else {
+							hi[2] += 0.001f;
+							hi[3] -= 0.001f;
+							temp = glm::translate(temp, glm::vec3(hi[2], hi[3], 0.0f));
+						}
+					} else {
+						if(j > max/2) {
+							hi[4] -= 0.001f;
+							hi[5] += 0.001f;
+							temp = glm::translate(temp, glm::vec3(hi[4], hi[5], 0.0f));
+						} else {
+							hi[6] -= 0.001f;
+							hi[7] -= 0.001f;
+							temp = glm::translate(temp, glm::vec3(hi[6], hi[7], 0.0f));
+						}
+					}
+
+					glUniformMatrix4fv(iViewModelLoc, 1, GL_FALSE, glm::value_ptr(temp)); 
+					glDrawArrays(GL_TRIANGLES, 0, 3);
+				}
+			}
+		}
+
+		//
+
+		mCurrent = glm::scale(mViewModel, glm::vec3(0.1f, 0.1f, 0.1f));
+		mCurrent = glm::translate(mCurrent, glm::vec3(5.0f, -5.0f, 0.0f));
+
+		if(go) {	
+			for(int i = 0; i < 360; i++){
+				temp = mCurrent;
+	
+				circlex[i] += cos(i) * 0.02f * speed;
+				circley[i] += sin(i) * 0.02f * speed;
+				temp = glm::translate(temp, glm::vec3( circlex[i], circley[i], 0.0f));
+	
+		  	glUniformMatrix4fv(iViewModelLoc, 1, GL_FALSE, glm::value_ptr(temp)); 
+				glDrawArrays(GL_TRIANGLES, 0, 3);
+	
+			}
+		}
+
+		
+
+		// Lines (axis)
 		
 		glBindVertexArray(uiVAO[1]);
 		
@@ -293,6 +435,7 @@ void Control::handleEvents() {
 
 int main(int argc, char** argv)
 {
+	srand(time(NULL));
 	Control c;
 	c.setWidth(500);
 	c.setHeight(500);
--- shader.frag
+++ shader.frag
@@ -2,8 +2,12 @@
 
 	smooth in vec3 theColor;
 	out vec4 outputColor;
+	float rand() { 
+		return mod(gl_FragCoord.x, 1) / mod(gl_FragCoord.y, 1) * mod(gl_FragCoord.z, 1);
+	}
 
 	void main()
 	{
 		   outputColor = vec4(theColor, 1.0);
+		   //outputColor = vec4(1.0, rand(), rand(), 1.0);
 	}
