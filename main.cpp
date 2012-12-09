38052d972be47f6788baa3319c16b272000123fc
Michiel Vos <info@fastfox.nl>
2012-12-09 14:29:23 +0100

    Clean code and remove files

--- main.cpp
+++ main.cpp
@@ -25,81 +25,51 @@ struct particle {
   float alpha;
   float size; 
 	float age;
-	bool isNewest;
 	bool exploded;
 	float explodetime;
 	float lifetime;
 	float timebeforelaunch;
+	float pointSize;
 	particle() {
-		location = glm::vec3( (float)(rand() % 3), 0.0f, 0.0f);
 		exploded = false;
-		explodetime = 1.0f;
+		explodetime = (float)((rand() % 10)) / 10.f + 1.0f; // Explode between 1 and 2 seconds.
 		age = 0.0f;
-		lifetime = 5.0f;
+		lifetime = 4.0f;
 		timebeforelaunch = 1.0f;
-		isNewest = true;
-		velocity = glm::vec3(0.0f, 4.0f, 0.0f);
 		gravity = glm::vec3(0.0f, -1.0f, 0.0f);
-	}
-	void normalizeVelocity(float s) {
-		velocity = glm::normalize(velocity) * s;
-	}
-	void randomVelocity(float min, float max) {
-		velocity = glm::vec3(
-		(float)( rand() % ( (int)(max - min) * 100 ) ) / 100.f + min,
-		(float)( rand() % ( (int)(max - min) * 100 ) ) / 100.f + min,
-		(float)( rand() % ( (int)(max - min) * 100 ) ) / 100.f + min );
-		//velocity.z = 0.0f;
-
+		color.w = 0.0f;
+		pointSize = 8.0f;
 	}
 };
 
 class Firework {
 	private:
 		std::list<particle> particles;
-		GLuint iViewModelLoc, iColorLoc, iVertexLoc, iPointSize;
+		GLuint iViewModelLoc, iColorLoc, iVertexLoc, iPointSizeLoc;
 		int age;
-		glm::vec3 gravity;
-
-
+		glm::vec4 randColor(float alpha);
+		glm::vec3 randomVelocity(float min, float max);
 	public:
 		Firework();
-		void insert(std::list<particle>::iterator i, particle p);
+		void add();
 		void calc(float dt);
 		void draw();
 		void setLocations(GLuint vm, GLuint c, GLuint v, GLuint p);
-		glm::vec4 randColor();
+		bool z; // z enabled?
 };
 
 Firework::Firework() {
-	float age = 0.0f;
-	glm::vec3 origin = glm::vec3(0.0f, 0.0f, 0.0f);
-
-	
-	for(int i = 0; i < 10; i++) {
-		particle p;
-		p.timebeforelaunch = (float)i /10.0f;
-		//p.location = glm::vec3((float)i / 2.0f, 0.0f, 0.0f);
-		//p.velocity = glm::vec3( (float)(rand() % 300) / 100.0f - 1.5f, 4.0f, (float)(rand() % 300) / 100.0f - 1.5f);
-		p.randomVelocity(2.0f, 5.0f);	
-		p.velocity.y += 6.0f;		
-		//p.color = glm::vec4(1.0f, 1.0f, 0.0f, 1.0f);
-		p.color = randColor();
-		particles.push_back(p);
-	}
-
-
-	//p = p.copy();
-	//p.location.y += 0.01f;
-	//pushP(p);
+	z = true;
+	add();
 }
 
-void Firework::insert(std::list<particle>::iterator i, particle p) {
-	particles.back().isNewest = false;
-//	particles.push_back(p);
-	particles.insert(i, p);
-	//std::cout << p.location.y << std::endl;
-	//std::cout << particles.size() << std::endl;
+void Firework::add() {
+	particle p;
+	p.location = glm::vec3( (float)(rand() % 4) - 2.0f, 0.0f, 0.0f);
+	p.velocity = randomVelocity(-2.0f, 2.0f);
+	p.velocity.y += 6.0f;		
+	p.color = randColor(0.0f);
+	particles.push_back(p);
 }
 
 void Firework::calc(float dt) {
@@ -113,42 +83,25 @@ void Firework::calc(float dt) {
 				i->velocity *= 0.99f;
 				i->gravity *= 1.01f;
 
-				//i->velocity *= 0.0f;
-				//i->gravity *= 0.0f;
-				
-				// Fading away
-				if(i->age > i->lifetime ) {
+				if(i->age > i->lifetime ) { // Remove old particles
 					i = particles.erase(i);	
-				} else if(i->exploded == false && i->age > i->explodetime) {
+				} else if(i->exploded == false && i->age > i->explodetime) { // time for explosion when not yet exploded
 					i->exploded = true;
-					//std::cout << "explode" << std::endl;
-					//if(i->location == NULL) {
-						//std::cout << "NULL" << std::endl;
-					//}
-					//std::cout << i->location.x << " " << i->location.y << " " << i->location.z << std::endl;
-					//std::cout << "after location cout" << std::endl;
-
 					
-					int salvos = rand() % 3 + 1; 
+					int salvos = rand() % 3 + 1; // amount of shots at explosion, with delay between shots.
 					int temp;
-					glm::vec4 c[2];
+					glm::vec4 c[2]; // Two colors
 				 	c[0] = i->color;	
-				 	c[1] = randColor();	
-					//std::cout << salvos << std::endl;
+				 	c[1] = randColor(0.0f);	
 					for(int a = 0; a < rand() % 100 + 500; a++ ) {
 						particle n;
 						n.location = i->location;
 
-						//std::cout << "before velocity a" << std::endl;
-						//in.velocity = 0.2f * i->velocity + randVelocity(i->location);
-						//n.velocity = 1.0f * i->velocity;
-						//std::cout << "before velocity b" << std::endl;
-					 
-						//n.gravity = glm::vec3(0.0f, 0.0f, 0.0f);
-						//n.velocity += randVelocity(i->location);
-
-						n.randomVelocity(-2.0, 2.0f);
-						n.normalizeVelocity(3.0f);
+						n.velocity = randomVelocity(-2.0f, 2.0f);
+						if(z == false) {
+							n.velocity.z = 0.0f;
+						}
+						n.velocity = glm::normalize(n.velocity) * 3.0f;
 						n.velocity += 0.5f * i->velocity;
 						
 						if(salvos == 1) {
@@ -157,44 +110,24 @@ void Firework::calc(float dt) {
 						} else {
 							temp = rand() % salvos;
 							n.timebeforelaunch = temp * 0.35f;
-							n.color = c[temp % 2];
+							n.color = c[temp % 2]; // Alternating colors
 						}
-						
-						//n.velocity += randVelocity(i->location.x, i->location.y, i->location.z);
-						//n.velocity = glm::vec3( (float)(rand() % 300) / 100.0f - 1.5f, 4.0f, (float)(rand() % 300) / 100.0f - 1.5f);
-						//std::cout << "after velocity" << std::endl;
 
 						n.lifetime = (float)(rand() % 4);
-						//std::cout << n.velocity.x << std::endl;
-						//std::cout << n.lifetime << std::endl;
 						n.exploded = true;
-						//n.color = randColor();
-						n.color.w = 0.0f;
-						insert(i, n);
+
+						n.color.w = 0.0f; // Make invisible till launch
+						n.pointSize = (float)(rand() % 30) / 10.0f + 1.0f;
+						particles.insert(i, n);
 					}
 					i = particles.erase(i);	
-					//std::cout << particles.size() << std::endl;
 				
-					particle p;
-					p.timebeforelaunch = 1.0f;
-					p.location = glm::vec3(0.0f, 0.0f, 0.0f);
-					//p.velocity = glm::vec3( (float)(rand() % 300) / 100.0f - 1.5f, 4.0f, (float)(rand() % 300) / 100.0f - 1.5f);
-					//p.velocity = glm::vec3(0.0f, 4.0f, 0.0f);
-					p.randomVelocity(-5.0f, 5.0f);	
-					p.velocity.y = abs(p.velocity.y) + 4.0f;		
-					//p.randomVelocity(-5.0f, 5.0f);	
-					//p.velocity.y = abs(p.velocity.y) + 2.0f;		
-					//p.color = glm::vec4(1.0f, 1.0f, 0.0f, 1.0f);
-					p.color = randColor();
-					particles.push_back(p);
+					add();
 
 				}	else {
-					//if( i->exploded == false) {
-						i->location += (i->velocity + i->gravity) * dt;
-						i->color.w = 1 - i->age / i->lifetime;
-					//}
+					i->location += (i->velocity + i->gravity) * dt;
+					i->color.w = 1 - i->age / i->lifetime;
 				}
-				//std::cout << i->color.w << std::endl;
 		}
 	}
 }
@@ -204,7 +137,7 @@ void Firework::draw() {
 	for(i = particles.begin(); i != particles.end(); i++) {
 			glUniform4f(iColorLoc, i->color.x, i->color.y, i->color.z, i->color.w);
 			glUniform3f(iVertexLoc, i->location.x, i->location.y, i->location.z);
-			//glUniformVec3f(iVertexLoc, i->location);
+			glUniform1f(iPointSizeLoc, i->pointSize);
 			glDrawArrays(GL_POINTS, 0, 2);
 	}
 }
@@ -213,52 +146,39 @@ void Firework::setLocations(GLuint vm, GLuint c, GLuint v, GLuint p) {
 		iViewModelLoc = vm;
 		iColorLoc = c;
 	 	iVertexLoc = v;
-	 	iPointSize = p;
+	 	iPointSizeLoc = p;
 }
  
-glm::vec4 Firework::randColor() {
-	return glm::vec4( fRand(), fRand(), fRand(), 1.0f);
+glm::vec4 Firework::randColor(float alpha) {
+	return glm::vec4( fRand(), fRand(), fRand(), alpha);
 }
 
+glm::vec3 Firework::randomVelocity(float min, float max) {
+	return glm::vec3(
+		(float)( rand() % ( (int)(max - min) * 100 ) ) / 100.f + min,
+		(float)( rand() % ( (int)(max - min) * 100 ) ) / 100.f + min,
+		(float)( rand() % ( (int)(max - min) * 100 ) ) / 100.f + min );
+}
 
 class Control {
 	private:
-		void handleEvents();
 		int iWidth;
 		int iHeight;
 		bool running;
 		GLuint iProjLoc, iViewModelLoc, iColorLoc, iVertexLoc, iPointSizeLoc;
 		glm::mat4 mProj, mViewModel;
 		GLuint programID;
-		GLuint uiVAO[3];
-		GLuint uiVBO[7];
-		GLuint dataBufferID;
-		GLint vertexLoc;
-		GLint colorLoc;
-		float color[3];
 		glm::vec3 eye, center;
     Firework fw;
-		//Uint32 lastTime, newTime;
-
 
 	public:
-		Control();
 		void initialize();
 		void deinitialize();
 		void prepare();
 		void run();
-		void render();
 		void setWidth(int w);
 		void setHeight(int h);
 };
-
-// Public
-
-Control::Control() {
-
-}
-
-
 
 void Control::initialize() {
 	// SDL
@@ -275,7 +195,6 @@ void Control::initialize() {
 	glViewport(0, 0, iWidth, iHeight);
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
 	glEnable(GL_DEPTH_TEST);
-	//glEnable(GL_CULL_FACE);
 	glDepthMask(GL_TRUE);
 	glClearDepth(1.0f);
   glEnable(GL_VERTEX_PROGRAM_POINT_SIZE);
@@ -287,98 +206,29 @@ void Control::initialize() {
 }
 
 void Control::deinitialize() {
-	
-	glDeleteBuffers(4, uiVBO);
-	glDeleteVertexArrays(2, uiVAO);
-
 	SDL_Quit();
 }
 
 void Control::prepare() {
-	//GLuint uiVAO[2];
-	//GLuint uiVBO[4];
 	running = true;
 
-	glGenVertexArrays(4, uiVAO);
-	glGenBuffers(7, uiVBO); 
-
-	// Triangle
-	
-	/*
-	glBindVertexArray(uiVAO[0]);
-
-	glBindBuffer(GL_ARRAY_BUFFER, uiVBO[0]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(fTriangle), fTriangle, GL_STATIC_DRAW); 
-	glEnableVertexAttribArray(0);
- 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	*/	
-
-	// Line
-	
-	glBindVertexArray(uiVAO[1]);
-	
-	glBindBuffer(GL_ARRAY_BUFFER, uiVBO[2]);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(fLine), fLine, GL_STATIC_DRAW); 
-	//glEnableVertexAttribArray(0);
- 	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	
-
-  // Particle
-
-  //float fParticle[] = { 1.0f, 1.0f, 1.0f, 2.0f, 2.0f, 2.0f };
-
-	glBindVertexArray(uiVAO[2]);
-	
-	//glBindBuffer(GL_ARRAY_BUFFER, uiVBO[5]);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(fParticle), fParticle, GL_STATIC_DRAW); 
-	glEnableVertexAttribArray(0);
- 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
-  
-
-
 	programID = LoadShaders("shader.vert", "shader.frag");
-
 	glUseProgram(programID);
-	//vertexLoc = glGetAttribLocation(programID, "inPosition");
-	//colorLoc = glGetAttribLocation(programID, "inColor");
-	//glUseProgram(0);
 
 	iProjLoc = glGetUniformLocation(programID, "projectionMatrix");
 	iViewModelLoc = glGetUniformLocation(programID, "viewMatrix");
 	iColorLoc = glGetUniformLocation(programID, "color");
 	iVertexLoc = glGetUniformLocation(programID, "inPosition");
 	iPointSizeLoc = glGetUniformLocation(programID, "pointSize");
+
 	fw.setLocations(iViewModelLoc, iColorLoc, iVertexLoc, iPointSizeLoc);
 
 	eye = glm::vec3(-1.0f, 1.0f, 12.0f);
 	center = glm::vec3(0.0f, 2.0f, 0.0f);
-	
-	
 }
 
 void Control::run() {
-	render();
-}
-
-void Control::render() {
-	
-
-
-	float up = 0.0f;
-	float rotation = 0.0f;
-	float y[10];
-	float x[10];
-	
-	int newX, newY;
-	float speed = 1.0f;
-
-
-
-	float fCamRoty = 0.0f;
-	float fCamZ = 5.0f;
-
-		
+	float camRoty = 0.0f;
 	int lasttime = SDL_GetTicks();
 	int newtime;
 
@@ -390,45 +240,57 @@ void Control::render() {
 					running = false;  
 					break;
 				case SDL_KEYDOWN: 
-					if(event.key.keysym.sym == SDLK_ESCAPE) {
-						running = false;
-					} else if(event.key.keysym.sym == SDLK_LEFT) {
-						//rotation -= 5.0f;
-						center.x -= 0.1f;
-					} else if(event.key.keysym.sym == SDLK_RIGHT) {
-						//rotation += 5.0f;
-						center.x += 0.1f;
-					} else if(event.key.keysym.sym == SDLK_UP) {
-						//up += 0.15f;
-						center.y += 0.1f;
-					} else if(event.key.keysym.sym == SDLK_DOWN) {
-						//up -= 0.15f;
-						center.y -= 0.1f;
-					}	else if(event.key.keysym.sym == 'a') {
-						fCamRoty += 1.0f;
-						//eye.x -= 0.1f;
-					} else if(event.key.keysym.sym == 'd') {
-						fCamRoty -= 1.0f;
-						//eye.x += 0.1f;
-					} else if(event.key.keysym.sym == 'w') {
-						eye.z -= .1;
-					} else if(event.key.keysym.sym == 's') {
-						eye.z += .1f;
-					} else if(event.key.keysym.sym == 'g') {
-					} else if(event.key.keysym.sym == 'r') {
-						eye = glm::vec3(-1.0f, 1.0f, 5.0f);
-						center = glm::vec3(0.0f, 0.0f, 0.0f);
+					switch(event.key.keysym.sym) {
+						case SDLK_ESCAPE:
+							running = false;
+							break;
+						case SDLK_LEFT:
+							center.x -= 0.1f;
+							break;
+						case SDLK_RIGHT:
+							center.x += 0.1f;
+							break;
+						case SDLK_UP:
+							center.y += 0.1f;
+							break;
+						case SDLK_DOWN:
+							center.y -= 0.1f;
+							break;
+						case 'a':
+							camRoty += 1.0f;
+							break;
+						case 'd':
+							camRoty -= 1.0f;
+							break;
+						case 'w':
+							eye.z -= 0.1f;
+							break;
+						case 's':
+							eye.z += 0.1f;
+							break;
 					}
-					//std::cout << event.key.keysym.sym << std::endl;
 					break;
+				case SDL_KEYUP:
+					switch(event.key.keysym.sym) {
+						case 'z':
+							fw.z = !fw.z;
+							break;
+						case 'm':
+							fw.add();
+							break;
+						case 'r':
+							eye = glm::vec3(-1.0f, 1.0f, 12.0f);
+							center = glm::vec3(0.0f, 2.0f, 0.0f);
+							break;
+					}	
+					break;
+				}
 			}
-		}
 
 
 		mViewModel = glm::lookAt(eye, center, glm::vec3(0.0f, 1.0f, 0.0f));
-		mViewModel = glm::rotate(mViewModel, fCamRoty, glm::vec3(0.0f, 1.0f, 0.0f));
+		mViewModel = glm::rotate(mViewModel, camRoty, glm::vec3(0.0f, 1.0f, 0.0f));
 		mProj = glm::perspective(45.0f, (float) iWidth / (float) iHeight, 0.1f, 100.0f);
-
 
 		glClearAccum(0.0f, 0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
@@ -436,15 +298,12 @@ void Control::render() {
 		glUniformMatrix4fv(iProjLoc, 1, GL_FALSE, glm::value_ptr(mProj));
 		glUniformMatrix4fv(iViewModelLoc, 1, GL_FALSE, glm::value_ptr(mViewModel));
 
-		glm::mat4 mCurrent, temp;
-
 
 		// Trials
 		glAccum(GL_RETURN, 0.95f);
 		glClear(GL_ACCUM_BUFFER_BIT);
 
 		// Lines (axis)
-   
 
 		glUniform1f(iPointSizeLoc, 1.0f);
 
@@ -465,52 +324,15 @@ void Control::render() {
 		}
 
 
-	/*
-
-	glUniform3f(iColorLoc, 1.0f, 0.0f, 0.0f);
-	for(float i = -10.0f; i < 10.0f; i++) {	
-		mCurrent = glm::translate(mViewModel, glm::vec3(i / 10.0f, .0f, 0.0f));
-		glUniformMatrix4fv(iViewModelLoc, 1, GL_FALSE, glm::value_ptr(mCurrent)); 
-		glDrawArrays(GL_POINTS, 0, 2);
-	} 
-
-	*/
-
-
-
-
     // Firework
 
-
-		glUniform1f(iPointSizeLoc, 2.0f);
-
-
 		newtime = SDL_GetTicks();
-		fw.calc((newtime - lasttime)/1000.0f);
+		fw.calc((newtime - lasttime) / 1000.0f);
 		lasttime = newtime;
-
 		fw.draw();
 
-		//glUniform3f(iVertexLoc, 1.0f, 1.0f, 0.0f);
-		//glDrawArrays(GL_POINTS, 0, 2);
-
-		// Test
-
-
-
-		//glUseProgram(programID);
-		//glBindVertexArray(uiVAO[2]);
-
-	  //glUniformMatrix4fv(iViewModelLoc, 1, GL_FALSE, glm::value_ptr(mCurrent)); 
-		//glBindBuffer(GL_ARRAY_BUFFER, dataBufferID);
-		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), 0);
-		//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), 0);
-    //glEnableVertexAttribArray(0);
-    //glEnableVertexAttribArray(1);
-		//glDrawArrays(GL_LINES, 0, 2);
-		
 		SDL_GL_SwapBuffers();
-		glAccum(GL_ACCUM, 0.9f);
+		glAccum(GL_ACCUM, 0.90f);
 	}
 }
 
@@ -520,12 +342,6 @@ void Control::setWidth(int w) {
 
 void Control::setHeight(int h) {
 	iHeight = h;
-}
-
-// Private
-
-void Control::handleEvents() {
-
 }
 
 int main(int argc, char** argv)
@@ -543,5 +359,3 @@ int main(int argc, char** argv)
 
 	return 0;
 }
-
-
